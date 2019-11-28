2999e64cb210e2e978b3a56a5551f83dd4bf434e
AdrianHeide <adrianheide@gmail.com>
2019-11-28 23:41:16 +0100

    Started Entry Point, get linking error

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Core.h"
+
+namespace Hazel {
+	class Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+}
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,17 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application {
+public:
+	Sandbox() {
+
+	}
+	~Sandbox() {
+
+	}
+};
+
+int main() {
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+	delete sandbox;
+}
